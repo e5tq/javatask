3b1d258e07abdbb70fd93a40db05554df527d5f6
e5tq <kiyoren061212@gmail.com>
2025-05-05 11:32:33 +0900

    Update GenderSpeaker.java

--- GenderSpeaker.java
+++ GenderSpeaker.java
@@ -4,10 +4,10 @@ public class GenderSpeaker {
     public static void main(String[] args) {
         AbstractPerson man = new Man();
         AbstractPerson woman = new Woman();
-
+        
         doSpeak(man);
         doSpeak(woman);
-    }
+    }    
 
     static void doSpeak(AbstractPerson person) {
         person.speak();
